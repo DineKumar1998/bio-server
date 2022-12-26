6215259100863a41272145993350798f210e0547
DineKumar1998 <91127479+DineKumar1998@users.noreply.github.com>
2022-12-26 12:08:53 +0530

    added cors

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const transporter = require('./transport');
+const cors = require('cors')
 
 const app = express();
 
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 
